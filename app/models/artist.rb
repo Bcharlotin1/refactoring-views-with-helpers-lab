d6b539d58307f54cd5b45c6d13e19f9b9a470ad8
bcharlotin1 <biancacharlotin1@gmail.com>
2021-07-27 14:06:23 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,13 +2,4 @@ class Artist < ActiveRecord::Base
   has_many :songs
   validates :name,  uniqueness: true
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name)
-  end
-
-  def artist_name
-    
-     self.artist ? self.artist.name : nil
-  end
-
 end
